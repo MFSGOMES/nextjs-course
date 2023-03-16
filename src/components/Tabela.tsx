f056724411f7e216cfa276767c8a389a8740863b
Marcos Felipe da Silva Gomes <marcos.gomes@grupoicts.com.br>
2023-03-16 10:36:24 -0400

    Adding actions o client list and .gitignore

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -1,9 +1,14 @@
 import Cliente from "@/core/Cliente";
+import { IconeDelete, IconeEdicao } from "./Icones";
 
 interface TabelaProps {
     clientes: Cliente[]
+    clienteSelecionado?: (cliente: Cliente) => void
+    clienteExcluido?: (cliente: Cliente) => void
 }
 export default function Tabela(props: TabelaProps) {
+
+    const exibirAcoes = props.clienteSelecionado || props.clienteExcluido
 
     function renderizarCbecalho() {
         return (
@@ -11,6 +16,7 @@ export default function Tabela(props: TabelaProps) {
                 <th className="text-left p-4">Código</th>
                 <th className="text-left p-4">Nome</th>
                 <th className="text-left p-4">Idade</th>
+                {exibirAcoes ? <th className="p-4">Ações</th> : false}
             </tr>
         )
     }
@@ -20,14 +26,49 @@ export default function Tabela(props: TabelaProps) {
             return (
                 <tr
                     key={cliente.id}
-                    className={`${i % 2 === 0 ? 'bg-purple-200': 'bg-purple-100'}`}
+                    className={`${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}
                 >
                     <td className="text-left p-4">{cliente.id}</td>
                     <td className="text-left p-4">{cliente.name}</td>
                     <td className="text-left p-4">{cliente.idade}</td>
+                    {exibirAcoes ? renderizarAcoes(cliente) : false}
                 </tr>
             )
         })
+    }
+
+    function renderizarAcoes(cliente: Cliente) {
+        return (
+            <td className="flex justify-center">
+                {props.clienteSelecionado
+                    ? (
+                        <button className={`
+                            flex justify-center items-center
+                            text-green-600 rounded-full p-2 m-1
+                            hover:bg-purple-50
+                        `}
+                        onClick={() => props.clienteSelecionado?.(cliente)}
+                        >
+                            {IconeEdicao}
+                        </button>
+                    )
+                    : false}
+
+                {props.clienteExcluido
+                    ? (
+                        <button className={`
+                            flex justify-center items-center
+                            text-red-500 rounded-full p-2 m-1
+                            hover:bg-purple-50
+                        `}
+                        onClick={() => props.clienteExcluido?.(cliente)}
+                        >
+                            {IconeDelete}
+                        </button>
+                    )
+                    : false}
+            </td>
+        )
     }
 
     return (
